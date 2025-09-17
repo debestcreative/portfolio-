b5d75bbe062e33af04d61267486c855932625c56
NUESAuniuyo <anthonyetimfavour@gmail.com>
2025-09-17 19:18:29 +0100

    dbest

--- script.js
+++ script.js
@@ -25,3 +25,33 @@
       });
   });
  
+
+
+
+  // Select modal elements
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImg");
+  const closeModal = document.getElementById("closeModal");
+
+  // Add click event to portfolio images
+  document.querySelectorAll(".portfolio-img").forEach(img => {
+    img.addEventListener("click", () => {
+      modal.classList.remove("hidden");
+      modal.classList.add("flex");
+      modalImg.src = img.src;
+    });
+  });
+
+  // Close modal when clicking the X
+  closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  });
+
+  // Close modal when clicking outside the image
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.add("hidden");
+      modal.classList.remove("flex");
+    }
+  });
